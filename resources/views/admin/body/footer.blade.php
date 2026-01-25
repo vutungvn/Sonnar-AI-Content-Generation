1056cb1169984faad073271710981309ff1882b2
vutungvn <tungvuvanthanh@gmail.com>
2026-01-25 21:01:19 +0700

    feat: update footer.blade.php

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -4,8 +4,8 @@
             <div class="col fs-13 text-muted text-center">
                 &copy;
                 <script>document.write(new Date().getFullYear())</script> - Made with <span
-                    class="mdi mdi-heart text-danger"></span> by <a href="#!"
-                    class="text-reset fw-semibold">Zoyothemes</a>
+                    class="mdi mdi-heart text-danger"></span> by <a href="#!" class="text-reset fw-semibold">Thanh
+                    Tùng</a>
             </div>
         </div>
     </div>
