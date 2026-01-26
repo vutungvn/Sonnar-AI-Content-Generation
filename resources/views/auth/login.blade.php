a33eae6ea269a0aa9076fe383f6891b4f0897d37
vutungvn <tungvuvanthanh@gmail.com>
2026-01-26 22:16:26 +0700

    feat: Customize Register Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -85,7 +85,7 @@
 
                                     <div class="text-center text-muted mb-4">
                                         <p class="mb-0">Don't have an account ?<a class='text-primary ms-2 fw-medium'
-                                                href='{{ route('register') }}'>Sing up</a></p>
+                                                href='{{ route('register') }}'>Sign up</a></p>
                                     </div>
 
                                     <div class="row">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,136 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<head>
+
+    <meta charset="utf-8" />
+    <title>Register Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
+    <meta name="author" content="Zoyothemes" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+
+    <!-- App css -->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="bg-white">
+
+    <!-- Begin page -->
+    <div class="account-page">
+        <div class="container-fluid p-0">
+            <div class="row align-items-center g-0">
+                <div class="col-xl-5">
+                    <div class="row">
+                        <div class="col-md-7 mx-auto">
+                            <div class="mb-0 border-0 p-md-5 p-lg-0 p-4">
+                                <div class="mb-4 p-0">
+                                    <a href="index.html" class="auth-logo">
+                                        <img src="{{ asset('backend/assets/images/logo-dark.png') }}" alt="logo-dark"
+                                            class="mx-auto" height="28" />
+                                    </a>
+                                </div>
+
+                                <div class="pt-0">
+                                    <form method="POST" action="{{ route('register') }}" class=" my-4">
+                                        @csrf
+
+                                        <div class="form-group mb-3">
+                                            <label for="name" class="form-label">Name</label>
+                                            <input class="form-control" name="name" type="text" id="name" required=""
+                                                placeholder="Enter your name">
+                                        </div>
+
+                                        <div class="form-group mb-3">
+                                            <label for="email" class="form-label">Email address</label>
+                                            <input class="form-control" type="email" id="email" name="email" required=""
+                                                placeholder="Enter your email">
+                                        </div>
+
+
+                                        <div class="form-group mb-3">
+                                            <label for="password" class="form-label">Password</label>
+                                            <input class="form-control" type="password" required="" id="password"
+                                                name="password" placeholder="Enter your password">
+                                        </div>
+
+                                        <div class="form-group mb-3">
+                                            <label for="password_confirmation" class="form-label">Confirm
+                                                Password</label>
+                                            <input class="form-control" type="password" required=""
+                                                id="password_confirmation" name="password_confirmation"
+                                                placeholder="Enter your confirm password">
+                                        </div>
+
+                                        <div class="form-group d-flex mb-3">
+                                            <div class="col-12">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="checkbox-signin">
+                                                    <label class="form-check-label" for="checkbox-signin">I agree to the
+                                                        <a href="#" class="text-primary fw-medium"> Terms and
+                                                            Conditions</a></label>
+                                                </div>
+                                            </div><!--end col-->
+                                        </div>
+
+                                        <div class="form-group mb-0 row">
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <button class="btn btn-primary" type="submit"> Register</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <div class="saprator my-4"><span>or sign in with</span></div>
+
+                                    <div class="text-center text-muted mb-4">
+                                        <p class="mb-0">Already have an account ?<a class='text-primary ms-2 fw-medium'
+                                                href='{{ route('login') }}'>Login here</a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-7">
+                    <div class="account-page-bg p-md-5 p-4">
+                        <div class="text-center">
+                            <h3 class="text-dark mb-3 pera-title">Quick, Effective, and Productive With Tapeli Admin
+                                Dashboard</h3>
+                            <div class="auth-image">
+                                <img src="{{ asset('backend/assets/images/authentication.svg') }}"
+                                    class="mx-auto img-fluid" alt="images">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+    </div>
+    <!-- END wrapper -->
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <!-- Vendor -->
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js') }}"></script>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- App js-->
+    <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+</body>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
