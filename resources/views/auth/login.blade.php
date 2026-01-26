3347d8f72e0754544d6e2326bd1f75546f8612eb
vutungvn <tungvuvanthanh@gmail.com>
2026-01-26 10:26:33 +0700

    feat: Customize Login Form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,161 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <meta charset="utf-8" />
+    <title>Log In Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc." />
+    <meta name="author" content="Zoyothemes" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+
+    <!-- App css -->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style" />
+
+    <!-- Icons -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="bg-white">
+    <!-- Begin page -->
+    <div class="account-page">
+        <div class="container-fluid p-0">
+            <div class="row align-items-center g-0">
+                <div class="col-xl-5">
+                    <div class="row">
+                        <div class="col-md-7 mx-auto">
+                            <div class="mb-0 border-0 p-md-5 p-lg-0 p-4">
+                                <div class="mb-4 p-0">
+                                    <a href="index.html" class="auth-logo">
+                                        <img src="{{ asset('backend/assets/images/logo-dark.png') }}" alt="logo-dark"
+                                            class="mx-auto" height="28" />
+                                    </a>
+                                </div>
+
+                                <div class="pt-0">
+                                    <form method="POST" action="{{ route('login') }}" class="my-4">
+                                        @csrf
+
+                                        <div class="form-group mb-3">
+                                            <label for="emailaddress" class="form-label">Email address</label>
+                                            <input class="form-control" type="email" id="email" name="email" required=""
+                                                placeholder="Enter your email">
+                                        </div>
+
+                                        <div class="form-group mb-3">
+                                            <label for="password" class="form-label">Password</label>
+                                            <input class="form-control" type="password" required="" id="password"
+                                                name="password" placeholder="Enter your password">
+                                        </div>
+
+                                        <div class="form-group d-flex mb-3">
+                                            <div class="col-sm-6">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" id="checkbox-signin"
+                                                        checked>
+                                                    <label class="form-check-label" for="checkbox-signin">Remember
+                                                        me</label>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-sm-6 text-end">
+                                                @if (Route::has('password.request'))
+                                                    <a class='text-muted fs-14'
+                                                        href='{{ route('password.request') }}'>Forgot
+                                                        password?</a>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group mb-0 row">
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <button class="btn btn-primary" type="submit"> Log In </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+
+                                    <div class="saprator my-4"><span>or sign in with</span></div>
+
+                                    <div class="text-center text-muted mb-4">
+                                        <p class="mb-0">Don't have an account ?<a class='text-primary ms-2 fw-medium'
+                                                href='{{ route('register') }}'>Sing up</a></p>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <a
+                                                class="btn text-dark border fw-normal d-flex align-items-center justify-content-center mb-3">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 48 48" class="me-2">
+                                                    <path fill="#ffc107"
+                                                        d="M43.611 20.083H42V20H24v8h11.303c-1.649 4.657-6.08 8-11.303 8c-6.627 0-12-5.373-12-12s5.373-12 12-12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C12.955 4 4 12.955 4 24s8.955 20 20 20s20-8.955 20-20c0-1.341-.138-2.65-.389-3.917" />
+                                                    <path fill="#ff3d00"
+                                                        d="m6.306 14.691l6.571 4.819C14.655 15.108 18.961 12 24 12c3.059 0 5.842 1.154 7.961 3.039l5.657-5.657C34.046 6.053 29.268 4 24 4C16.318 4 9.656 8.337 6.306 14.691" />
+                                                    <path fill="#4caf50"
+                                                        d="M24 44c5.166 0 9.86-1.977 13.409-5.192l-6.19-5.238A11.91 11.91 0 0 1 24 36c-5.202 0-9.619-3.317-11.283-7.946l-6.522 5.025C9.505 39.556 16.227 44 24 44" />
+                                                    <path fill="#1976d2"
+                                                        d="M43.611 20.083H42V20H24v8h11.303a12.04 12.04 0 0 1-4.087 5.571l.003-.002l6.19 5.238C36.971 39.205 44 34 44 24c0-1.341-.138-2.65-.389-3.917" />
+                                                </svg>
+                                                <span>Sign in with Google</span>
+                                            </a>
+                                        </div>
+
+                                        <div class="col-12">
+                                            <a
+                                                class="btn btn-primary fw-normal d-flex align-items-center justify-content-center mb-3">
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                    viewBox="0 0 24 24" class="me-2">
+                                                    <path fill="#ffffff"
+                                                        d="M9.198 21.5h4v-8.01h3.604l.396-3.98h-4V7.5a1 1 0 0 1 1-1h3v-4h-3a5 5 0 0 0-5 5v2.01h-2l-.396 3.98h2.396z" />
+                                                </svg>
+                                                <span>Sign in with Facebook</span>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-7">
+                    <div class="account-page-bg p-md-5 p-4">
+                        <div class="text-center">
+                            <h3 class="text-dark mb-3 pera-title">Quick, Effective, and Productive With Tapeli Admin
+                                Dashboard</h3>
+                            <div class="auth-image">
+                                <img src="{{ asset('backend/assets/images/authentication.svg')}}"
+                                    class="mx-auto img-fluid" alt="images">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <!-- END wrapper -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <!-- Vendor -->
+    <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/jquery.counterup/jquery.counterup.min.js')}}"></script>
+    <script src="{{ asset('backend/assets/libs/feather-icons/feather.min.js')}}"></script>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <!-- App js-->
+    <script src="{{ asset('backend/assets/js/app.js')}}"></script>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+</body>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
