99df9db453828f636e5f49dacc82dbaacbe1ac95
vutungvn <tungvuvanthanh@gmail.com>
2026-02-13 21:27:42 +0700

    feat(reviews table): create reviews table in database

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -31,16 +31,16 @@
                 <li>
                     <a href="#sidebarAuth" data-bs-toggle="collapse">
                         <i data-feather="users"></i>
-                        <span> Authentication </span>
+                        <span> Review Management </span>
                         <span class="menu-arrow"></span>
                     </a>
                     <div class="collapse" id="sidebarAuth">
                         <ul class="nav-second-level">
                             <li>
-                                <a href="auth-login.html" class="tp-link">Log In</a>
+                                <a href="auth-login.html" class="tp-link">All Review</a>
                             </li>
                             <li>
-                                <a href="auth-register.html" class="tp-link">Register</a>
+                                <a href="auth-register.html" class="tp-link">Add Review</a>
                             </li>
                         </ul>
                     </div>
