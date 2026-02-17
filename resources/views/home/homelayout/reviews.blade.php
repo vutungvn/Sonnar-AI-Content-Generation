3e9ab11ef18b3ee5d65556997ad742ebf2898d47
vutungvn <tungvuvanthanh@gmail.com>
2026-02-17 21:21:22 +0700

    feat(reviews table): show review of homepage

--- resources/views/home/homelayout/reviews.blade.php
+++ resources/views/home/homelayout/reviews.blade.php
@@ -14,97 +14,30 @@
         </div>
     </div>
     <div class="lonyo-testimonial-slider-init">
-        <div class="lonyo-t-wrap wrap2 light-bg">
-            <div class="lonyo-t-ratting">
-                <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
-            </div>
-            <div class="lonyo-t-text">
-                <p>"This app transformed my budgeting! It has been a clear view longer have to worry of my It has
-                    been success expenses and savings goals."</p>
-            </div>
-            <div class="lonyo-t-author">
-                <div class="lonyo-t-author-thumb">
-                    <img src="{{ asset('frontend/assets/images/v1/img7.png') }}" alt="">
-                </div>
-                <div class="lonyo-t-author-data">
-                    <p>Liam Gallagher</p>
-                    <span>Teacher of Luxe Escapes Hotels</span>
-                </div>
-            </div>
-        </div>
-        <div class="lonyo-t-wrap wrap2 light-bg">
-            <div class="lonyo-t-ratting">
-                <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
-            </div>
-            <div class="lonyo-t-text">
-                <p>“The interface is intuitive, and I love how syncs with my bank accounts. I no longer have to
-                    worry about manual tracking. Highly recommend!”</p>
-            </div>
-            <div class="lonyo-t-author">
-                <div class="lonyo-t-author-thumb">
-                    <img src="{{ asset('frontend/assets/images/v1/img2.png') }}" alt="">
-                </div>
-                <div class="lonyo-t-author-data">
-                    <p>Michael Chen</p>
-                    <span>Founder of EcoChic Apparel</span>
-                </div>
-            </div>
-        </div>
-        <div class="lonyo-t-wrap wrap2 light-bg">
-            <div class="lonyo-t-ratting">
-                <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
-            </div>
-            <div class="lonyo-t-text">
-                <p>“With this app, I’ve been able to stick to my budget and even save for a vacation.The budget
-                    alerts are a game changer!”</p>
-            </div>
-            <div class="lonyo-t-author">
-                <div class="lonyo-t-author-thumb">
-                    <img src="{{ asset('frontend/assets/images/v1/img3.png') }}" alt="">
-                </div>
-                <div class="lonyo-t-author-data">
-                    <p>David Nguyen</p>
-                    <span>COO of Luxe Escapes Hotels</span>
-                </div>
-            </div>
-        </div>
-        <div class="lonyo-t-wrap wrap2 light-bg">
-            <div class="lonyo-t-ratting">
-                <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
-            </div>
-            <div class="lonyo-t-text">
-                <p>"Having all my accounts in one place gives me complete overspending accounts control over my
-                    money. Highly recommend!"</p>
-            </div>
-            <div class="lonyo-t-author">
-                <div class="lonyo-t-author-thumb">
-                    <img src="{{ asset('frontend/assets/images/v1/img5.png') }}" alt="">
-                </div>
-                <div class="lonyo-t-author-data">
-                    <p>Rachel Kim</p>
-                    <span>CEO of GreenLeaf Organics</span>
-                </div>
-            </div>
-        </div>
-        <div class="lonyo-t-wrap wrap2 light-bg">
-            <div class="lonyo-t-ratting">
-                <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
-            </div>
-            <div class="lonyo-t-text">
-                <p>"Having all my accounts in one place gives me complete control over my money. So user-friendly
-                    and helpful! Highly recommend!"</p>
-            </div>
-            <div class="lonyo-t-author">
-                <div class="lonyo-t-author-thumb">
-                    <img src="{{ asset('frontend/assets/images/v1/img6.png') }}" alt="">
-                </div>
-                <div class="lonyo-t-author-data">
-                    <p>Aisha Hassan</p>
-                    <span>CEO of RoyexLeaf Organics</span>
-                </div>
-            </div>
-        </div>
 
+        @php
+            $reviews = App\Models\Review::latest()->get();
+        @endphp
+
+        @foreach ($reviews as $review)
+            <div class="lonyo-t-wrap wrap2 light-bg">
+                <div class="lonyo-t-ratting">
+                    <img src="{{ asset('frontend/assets/images/shape/star.svg') }}" alt="">
+                </div>
+                <div class="lonyo-t-text">
+                    <p>{{ $review->message }}</p>
+                </div>
+                <div class="lonyo-t-author">
+                    <div class="lonyo-t-author-thumb">
+                        <img src="{{ asset($review->image) }}" style="border-radius: 10px;" alt="">
+                    </div>
+                    <div class="lonyo-t-author-data">
+                        <p>{{ $review->name }}</p>
+                        <span>{{ $review->position }}</span>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
     <div class="lonyo-t-overlay2">
         <img src="{{ asset('frontend/assets/images/v2/overlay.png') }}" alt="">
