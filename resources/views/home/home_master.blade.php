274608de049f6fad13337488bbb00c28a3fb48ba
vutungvn <tungvuvanthanh@gmail.com>
2026-02-21 14:54:35 +0700

    feat(slider): add slider table into database

--- resources/views/home/home_master.blade.php
+++ resources/views/home/home_master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sonar AI</title>
+    <title>Easy - IT Solution</title>
 
     <link rel="shortcut icon" href="{{ asset('frontend/assets/images/favicon.ico') }}" type="image/x-icon">
     <link rel="icon" href="{{ asset('frontend/assets/images/favicon.ico') }}" type="image/x-icon">
