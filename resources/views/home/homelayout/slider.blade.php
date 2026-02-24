1810afa993da1b3885d3d0f3b4e44ceae0dfb66b
vutungvn <tungvuvanthanh@gmail.com>
2026-02-24 09:36:11 +0700

    feat(slider): show slider in homepage

--- resources/views/home/homelayout/slider.blade.php
+++ resources/views/home/homelayout/slider.blade.php
@@ -1,19 +1,22 @@
+@php
+    $slider = App\Models\Slider::find(2);
+@endphp
+
 <div class="lonyo-hero-section light-bg">
     <div class="container">
         <div class="row">
             <div class="col-lg-7 d-flex align-items-center">
                 <div class="lonyo-hero-content" data-aos="fade-up" data-aos-duration="700">
-                    <h1 class="hero-title">Manage your finances more effectively</h1>
-                    <p class="text">Track your daily finances automatically. Manage your money in a friendly &
-                        flexible way, making it easy to spend guilt-free.</p>
+                    <h1 class="hero-title">{{ $slider->title }}</h1>
+                    <p class="text">{{ $slider->description }}</p>
                     <div class="mt-50" data-aos="fade-up" data-aos-duration="900">
-                        <a href="sign-up.html" class="lonyo-default-btn hero-btn">Create a free account</a>
+                        <a href="{{ $slider->link }}" class="lonyo-default-btn hero-btn">Contact With Us</a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-5">
                 <div class="lonyo-hero-thumb" data-aos="fade-left" data-aos-duration="700">
-                    <img src="{{ asset('frontend/assets/images/v1/hero-thumb.png') }}" alt="">
+                    <img src="{{ asset($slider->image) }}" alt="">
                     <div class="lonyo-hero-shape">
                         <img src="{{ asset('frontend/assets/images/shape/hero-shape1.svg') }}" alt="">
                     </div>
