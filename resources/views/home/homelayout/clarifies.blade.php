9772c9913d2014aecf6a7d214cd0f6d5d067a4f7
vutungvn <tungvuvanthanh@gmail.com>
2026-03-13 13:41:51 +0700

    feat(clarifis): show dynamic data in homepage

--- resources/views/home/homelayout/clarifies.blade.php
+++ resources/views/home/homelayout/clarifies.blade.php
@@ -1,17 +1,21 @@
+@php
+    $clarify = App\Models\Clarifi::find(1);
+@endphp
+
 <section class="lonyo-section-padding6">
     <div class="container">
         <div class="row">
             <div class="col-lg-5">
                 <div class="lonyo-content-thumb" data-aos="fade-up" data-aos-duration="700">
-                    <img src="{{ asset('frontend/assets/images/v1/content-thumb.png') }}" alt="">
+                    <img src="{{ asset($clarify->image) }}" alt="{{ $clarify->title }}">
                 </div>
             </div>
             <div class="col-lg-7 d-flex align-items-center">
                 <div class="lonyo-default-content pl-50" data-aos="fade-up" data-aos-duration="700">
-                    <h2>It clarifies all strategic financial decisions</h2>
-                    <p class="data">With this tool, you can say goodbye to overspending, stay on track with your
-                        savings goals, and say goodbye to financial worries. Get ready for a clearer view of your
-                        finances like never before!</p>
+                    <h2 id="clarify-title" contenteditable="{{ auth()->check() ? 'true' : 'false' }}"
+                        data-id="{{ $clarify->id }}">{{ $clarify->title }}</h2>
+                    <p class="data" id="clarify-description" contenteditable="{{ auth()->check() ? 'true' : 'false' }}"
+                        data-id="{{ $clarify->id }}">{{ $clarify->description }}</p>
                     <div class="lonyo-faq-wrap1 mt-50">
                         <div class="lonyo-faq-item open" data-aos="fade-up" data-aos-duration="500">
                             <div class="lonyo-faq-header">
