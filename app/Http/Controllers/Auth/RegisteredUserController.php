dcfd40c82ebab6883c5f3b6e991498056cebfee8
vutungvn <tungvuvanthanh@gmail.com>
2026-03-15 17:34:13 +0700

    fix(auth): redirect login route when click register button in register form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -7,7 +7,6 @@ use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
 use Illuminate\View\View;
@@ -31,7 +30,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
@@ -43,8 +42,11 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        $notification = array(
+            'message' => 'Registration successful!',
+            'alert-type' => 'success'
+        );
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('login', absolute: false))->with($notification);
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,6 +19,8 @@
     <!-- Icons -->
     <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
 </head>
 
 <body class="bg-white">
@@ -148,6 +150,34 @@
 
     <!-- App js-->
     <script src="{{ asset('backend/assets/js/app.js') }}"></script>
+
+    <script>
+        @if(Session::has('message'))
+            toastr.options = {
+                "timeOut": "3000",
+                "positionClass": "toast-bottom-right",
+            }
+
+            var type = "{{ Session::get('alert-type', 'info') }}"
+            switch (type) {
+                case 'info':
+                    toastr.info(" {{ Session::get('message') }} ");
+                    break;
+
+                case 'success':
+                    toastr.success(" {{ Session::get('message') }} ");
+                    break;
+
+                case 'warning':
+                    toastr.warning(" {{ Session::get('message') }} ");
+                    break;
+
+                case 'error':
+                    toastr.error(" {{ Session::get('message') }} ");
+                    break;
+            }
+        @endif 
+    </script>
 </body>
 
 </html>
